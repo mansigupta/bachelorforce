8d63f5475bf1e2acbdd55ced11a4be29e7072af4
Chris Hemphill <chris@chris-hemphills-macbook.local>
2011-02-03 22:13:46 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bachelorforce::Application.config.secret_token = '551e6e5c345c4bd345c4e34557e70cd994a79059928d86979fac2b40250eb1270b19040049db8ce89468844e48dea821202e727155903cb788b21dd20ac778ba'
